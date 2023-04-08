f4f38b0d67fc5e61d7b54183bde22c3d4141de2e
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2023-04-07 22:32:04 -0300

    feat: Define handle de erros globais

--- src/http/controllers/register-controller.ts
+++ src/http/controllers/register-controller.ts
@@ -27,7 +27,7 @@ export async function registerController(
       return reply.status(409).send({message: error.message});
     }
     
-    return reply.status(500).send(); //TODO: Fix me, for new strategy
+    throw error;
   }
 
   return reply.status(201).send();
